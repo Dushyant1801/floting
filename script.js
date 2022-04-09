74e56e4af8bb90fb9ba9fc056f8118306e93a7a4
Dushyant Vikram singh <dushsingh20152016@gmail.com>
2022-04-10 03:33:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var navigation = document.querySelector('.navigation');
+document.querySelector('.toggle').onclick = function(){
+    this.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
